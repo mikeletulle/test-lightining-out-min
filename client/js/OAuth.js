1c6d6ac2f94e82a1f37348ccb2719f806dfb4f9e
rt-heroku <rodrigo.torres@mulesoft.com>
2022-12-07 14:28:30 -0500

    Changes made: 07/12/22 14:28:30

--- client/js/OAuth.js
+++ client/js/OAuth.js
@@ -3,7 +3,7 @@ $("#prodBtn").click(prodLogin);
 $("#sandBtn").click(sandLogin);
 
 var apiVersion = 'v37.0',
-    clientId = '3MVG9FMtW0XJDLd16RjTq6CPvecw28Vh0kS.DFi1XRMggLT9ftCSylqo6SslhCIkfczqXVNL_0w0c0GkgJtZk',
+    clientId = '3MVG9FMtW0XJDLd16RjTq6CPvecw28Vh0kS.6Cel800D8a000001Q8ut8888a000000h0LtNPXgIBgMyZvw8hDDQ9giA5nCSCilPJTDX83Vw6pfIB1BHHx0FRNvRR4JTURBi8tFV4BUYUtr',
     clientSecret = '7CE1CD08F5691B019ADD541F16218C48A030E96D61156DAFE80A6AD819D82FF9',
     loginUrl = 'https://login.salesforce.com/',
     redirectURI = "https://test-lightining-out.herokuapp.com/oauthcallback.html",
