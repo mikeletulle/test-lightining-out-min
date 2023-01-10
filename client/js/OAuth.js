d26137f5c973612eac8e85d3cda58626a71c8395
Mike Letulle <mletulle@yahoo.com>
2023-01-10 12:17:10 -0600

    added comments

--- client/js/OAuth.js
+++ client/js/OAuth.js
@@ -31,7 +31,8 @@ function getConfigAndLogin(call){
 
 function prodLogin()
 {
-	loginUrl = 'https://login.salesforce.com/'; 
+	//loginUrl = 'https://login.salesforce.com/'; 
+    loginUrl = 'https://sdodemo-main-166ce2cf6b6-172-180fc177bb0.force.com/LightningOutExample/login';
     getConfigAndLogin(login);
 }
 
